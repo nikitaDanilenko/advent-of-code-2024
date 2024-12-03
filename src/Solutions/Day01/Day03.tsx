807bb288fcf74dbde8eb4869c7c9f0ef0f9c34a6
Nikita Danilenko <nikita.danilenko@riskident.com>
2024-12-03 15:14:11 +0100

    Add solution for day03.2

--- src/Solutions/Day01/Day03.tsx
+++ src/Solutions/Day01/Day03.tsx
@@ -1,5 +1,6 @@
 import DayWith from "../Utils/DayUtil.tsx";
 import {sum} from "../Utils/MathUtil.ts";
+import {takeUntilRemainderStartsWith} from "../Utils/CollectionUtil.ts";
 
 const multiplicationRegExp = /mul\((\d+),(\d+)\)/g
 
@@ -11,36 +12,52 @@ function Day03() {
   }
 
   type PuzzleInput = {
-    multiplications: Multiplication[]
+    multiplications: Multiplication[][]
   }
 
 
-  function parseInput(puzzleInput: string): PuzzleInput {
-    const oneLine = puzzleInput.replace("\n", "")
-    const matches = oneLine.matchAll(multiplicationRegExp)
+  function parseInput(input: string): PuzzleInput {
+    const oneLine = input.replace("\n", "")
 
-    const multiplications =
-      Array.from(matches)
-        .map((match) => {
-          return {
-            first: BigInt(match[1]),
-            second: BigInt(match[2])
-          }
-        })
+    function iterateChunks(fragment1: string, fragment2: string, remainder: string, relevantBlocks: string[]): [string, string[]] {
+      if (remainder === "") {
+        return ["", relevantBlocks]
+      } else {
+        const [nextChunk, nextRemainder] = takeUntilRemainderStartsWith(fragment1, remainder)
+        return iterateChunks(fragment2, fragment1, nextRemainder, [...relevantBlocks, nextChunk])
+      }
+    }
 
+    const [_, relevantBlocks] = iterateChunks("don't()", "do()", oneLine, [])
+
+    const multiplications = relevantBlocks.map((block) => {
+      return Array.from(block.matchAll(multiplicationRegExp)).map((match) => {
+        return {
+          first: BigInt(match[1]),
+          second: BigInt(match[2])
+        }
+      })
+    })
 
     return {multiplications: multiplications}
   }
 
+  function sumOfMultiplications(multiplications: Multiplication[]): bigint {
+    return sum(multiplications.map((multiplication) => multiplication.first * multiplication.second))
+  }
+
   function solvePart1(puzzleInput: PuzzleInput): bigint {
-    const result =
-      sum(puzzleInput.multiplications
-        .map((multiplication) => multiplication.first * multiplication.second))
-    return result
+    // For the first part, all occurrences are fine.
+    return sumOfMultiplications(puzzleInput.multiplications.flat())
   }
 
   function solvePart2(puzzleInput: PuzzleInput): bigint {
-    return BigInt(0)
+    // For the second part only the even occurrences are relevant, because every odd occurrence starts with 'don't()'.
+    return sumOfMultiplications(
+      puzzleInput.multiplications
+        .filter((_, index) => index % 2 === 0)
+        .flat()
+    )
   }
 
   return DayWith(
---
+++ src/Solutions/Utils/CollectionUtil.ts
@@ -0,0 +1,7 @@
+export function takeUntilRemainderStartsWith(
+  fragment: string,
+  text: string,
+): [string, string]  {
+  const occurrence = text.indexOf(fragment)
+  return occurrence >= 0 ? [text.substring(0, occurrence), text.substring(occurrence)] : [text, ""];
+}
