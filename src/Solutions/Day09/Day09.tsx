6fb7dc68e06827c463fdb2ea78c12d4cd115661f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-12-10 23:28:06 +0100

    Add explanation

--- src/Solutions/Day09/Day09.tsx
+++ src/Solutions/Day09/Day09.tsx
@@ -33,6 +33,43 @@ function combineViaLoop(leftToRight: string[], rightToLeftNonEmpty: string[], ta
   const combined = []
   let reachedCells = 0
   while (reachedCells <= targetCells) {
+    /* Explanation:
+       First, with spaces. Imagine the disk array 0..111....22222
+       Now, go through it from left to right, and match it to its reversed array.
+
+       0..111....22222
+       22222....111..0
+
+       The first position is filled, hence we take it directly, i.e.
+
+       intermediate result: 0
+       ..111....22222
+       22222....111.. <- note that we can drop the last position, because it cannot fit anymore.
+
+       Now, we have two empty positions, and can take the two 2s in succession:
+
+       intermediate result: 022
+       111....22222
+       222....111..
+
+       Now, again, the leftmost positions are filled, and we can take them directly:
+
+       intermediate result: 022111
+       ....2222
+       222....1
+
+       Again, we have two empty positions, and can take the three 2s in succession:
+
+       intermediate result: 02211222
+
+       We now have as many cells as we need, and can stop.
+
+       Now notice that empty spaces in the right array are irrelevant, hence we can skip them.
+       Also, we do not need to do file part on its own, but rather can take whole blocks of numbers.
+       This may not seem like much, but since the blocks can be up to 9 numbers long,
+       assuming uniform distribution, using blocks we only have about 1/5th of all the iterations,
+       because blocks are about 5 cells long, and we handle all cells of a block at once.
+     */
     const nonEmptyLeft = lodash.takeWhile(leftToRight, x => x !== empty)
     if (nonEmptyLeft.length > 0) {
       leftToRight = lodash.drop(leftToRight, nonEmptyLeft.length)
