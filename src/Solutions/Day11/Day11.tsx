1ae66f5ec9f570ff68585d3e137a2b170d46f137
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-12-11 23:31:13 +0100

    Add solution for day 11.2, and remove original solution

--- src/Solutions/Day11/Day11.tsx
+++ src/Solutions/Day11/Day11.tsx
@@ -1,6 +1,7 @@
-import {Solution} from "../Utils/Types.ts";
-import DayWith from "../Utils/DayUtil.tsx";
-import lodash from "lodash";
+import {Solution} from "../Utils/Types.ts"
+import DayWith from "../Utils/DayUtil.tsx"
+import lodash from "lodash"
+import {applyN, sum} from "../Utils/MathUtil.ts"
 
 type Stone = bigint
 
@@ -14,38 +15,69 @@ function parse(input: string): PuzzleInput {
     .map((word) => BigInt(word))
 }
 
+// Not very pretty: One could double down on BigInt for the length part, but the indirection via string works fine.
 function next(stone: Stone): Stone[] {
   if (stone === BigInt(0)) {
     return [BigInt(1)]
-  }
-  else {
+  } else {
     const stoneString = stone.toString()
     const length = stoneString.length
     if (length % 2 === 0) {
       const half = length / 2
       return [BigInt(stoneString.slice(0, half)), BigInt(stoneString.slice(half))]
-    }
-    else {
+    } else {
       return [stone * BigInt(2024)]
     }
   }
 }
 
-function iterateNext(iterations: number, stones: Stone[]): Stone[] {
-  return lodash.range(iterations).reduce((acc, _) => {
-    console.log(acc)
-    return acc.flatMap(next)
-  }, stones)
+type Amounts = [string, number][]
+
+function computeViaAmount(iterations: number, stones: Stone[]): Amounts {
+
+  function blink(amounts: Amounts): Amounts {
+    const nextMap = new Map(amounts)
+    /* 1. Mutable implementation, because maps are most easily modified in this fashion.
+       2. The map is copied
+       3. The amount of stones in the result map is decreased by the current amount of stones.
+          It may seem like it's always setting to 0, but that is not true.
+          For example, say the stones are "11 1". Then the initial amount of 1s is 1.
+          Now we process the stone 11.
+          This leads to the number of 1s being 3.
+          Next we see the stone 1. The original amount is 1, but the current amount is 3.
+       4. In every iteration we thus reduce the amount of the currently processed stone by the original amount,
+          and increase the amounts of all result stones (at least 1, at most 2) by the original amount.
+     */
+    amounts.forEach(([stone, amount]) => {
+      const nextStones = next(BigInt(stone)).map(stone => stone.toString())
+      const currentAmount = nextMap.get(stone) ?? 0
+      if (amount > 0)
+        nextMap.set(stone, currentAmount - amount)
+      nextStones.forEach(nextStone => {
+        const currentAmount = nextMap.get(nextStone) ?? 0
+        nextMap.set(nextStone, currentAmount + amount)
+      })
+    })
+    return Array.from(nextMap.entries())
+  }
+
+  const amountMap =
+    stones.map(stone => [stone.toString(), 1] as [string, number])
+
+  return lodash.last(applyN(iterations + 1, blink, amountMap))!!
 }
 
 function solve(input: PuzzleInput): Solution<bigint> {
-  const stones = iterateNext(25, input).length
+  function withNIterations(n: number): bigint {
+    return sum(computeViaAmount(n, input).map(([, number]) => BigInt(number)))
+  }
 
-  const stones2 = BigInt(0)
+  const stones = withNIterations(25)
+  const stones2 = withNIterations(75)
 
   return {
-    part1: BigInt(stones),
-    part2: BigInt(stones2),
+    part1: stones,
+    part2: stones2
   }
 }
 
