d24bdd80a219cf589d3c68a05628dcb247e8e5d9
Nikita Danilenko <nikita.danilenko@riskident.com>
2024-12-13 15:39:17 +0100

    Add solution for day 13.1

--- src/Solutions/Day13/Day13.tsx
+++ src/Solutions/Day13/Day13.tsx
@@ -1,5 +1,6 @@
 import {Position2d, Solution} from "../Utils/Types.ts"
 import DayWith from "../Utils/DayUtil.tsx"
+import {sum} from "../Utils/MathUtil.ts";
 
 type Equation = {
   a: Position2d,
@@ -11,13 +12,14 @@ type PuzzleInput = {
   equations: Equation[]
 }
 
-const buttonARegExp = /Button A: X+(\d+), Y+(\d+)/g
-const buttonBRegExp = /Button B: X+(\d+), Y+(\d+)/g
+const buttonARegExp = /Button A: X\+(\d+), Y\+(\d+)/g
+const buttonBRegExp = /Button B: X\+(\d+), Y\+(\d+)/g
 const targetRegExp = /Prize: X=(\d+), Y=(\d+)/g
 
 function parsePositionFrom(line: string, regExp: RegExp): Position2d {
-  // Unsafe, but crashing is fine (error message is clear enough)
-  const matches = line.match(regExp)!!
+  // Unsafe, but crashing is fine (error message is clear enough).
+  // Afterthought: This is very ugly, and very unsafe! Why do matchAll, and match return such different results?
+  const matches = Array.from(line.matchAll(regExp))[0]
   return {
     x: parseInt(matches[1]),
     y: parseInt(matches[2])
@@ -44,9 +46,45 @@ function parse(input: string): PuzzleInput {
   }
 }
 
+/*
+  The function solves a linear equation with two variables.
+  We are solving r * x + s * y = t for the variables r and s, where x, y are two-dimensional vectors.
+  There is a closed solution for such equations, so this is the implementation.
+  The values originate from plugging in the inverse formula for a 2x2 matrix,
+  and using the correct arguments everywhere.
+
+  Also, since the solution needs to be integral, we need to check if the result is integral.
+  Since that may not be the case, the result may be undefined.
+ */
+function solveEquation(equation: Equation): [bigint, bigint] | undefined {
+  const denominatorR = equation.a.x * equation.b.y - equation.b.x * equation.a.y
+  const numeratorR = equation.b.y * equation.target.x - equation.b.x * equation.target.y
+
+  if (numeratorR % denominatorR === 0) {
+    const r = numeratorR / denominatorR
+    const numeratorS = equation.target.y - r * equation.a.y
+    if (numeratorS % equation.b.y === 0) {
+      const s = numeratorS / equation.b.y
+
+      return [BigInt(r), BigInt(s)]
+    } else return undefined
+  } else return undefined
+}
+
+// The "cheapest" and the "at most 100 times" are red herrings for the first part.
+function solve1(equations: Equation[]): bigint {
+  const solutions =
+    equations
+      .map(solveEquation)
+      .filter((solution) => solution !== undefined)
+      .map(([r, s]) => {return 3n * r + s})
+
+  return sum(solutions)
+}
+
 function solve(input: PuzzleInput): Solution<bigint> {
   return {
-    part1: 0n,
+    part1: solve1(input.equations),
     part2: 0n
   }
 }
