4deaaad85962b6f047c1345ba76b09b604998992
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-12-16 21:21:09 +0100

    Add basics for day 16

--- src/Solutions/Day16/Day16.tsx
+++ src/Solutions/Day16/Day16.tsx
@@ -1,10 +1,41 @@
-import { Solution } from '../Utils/Types.ts'
+import { Position2d, Solution } from '../Utils/Types.ts'
 import DayWith from '../Utils/DayUtil.tsx'
+import { parseStringPositionMap, StringPosition } from '../Utils/InputUtil.ts'
+import lodash from 'lodash'
 
-type PuzzleInput = string
+type PuzzleInput = {
+  start: Position2d
+  end: Position2d
+  maze: Map<StringPosition, Element>
+}
+
+enum Element {
+  Empty = '.',
+  Wall = '#'
+}
+
+function parseElement(char: string): Element {
+  return char === '#' ? Element.Wall : Element.Empty
+}
 
 function parse(input: string): PuzzleInput {
-  return input
+  const map = parseStringPositionMap(input)
+  const entries = Array.from(map.entries())
+
+  function findPosition(char: string): Position2d {
+    return JSON.parse(lodash.find(entries, ([, value]) => value === char)!![0]) as Position2d
+  }
+
+  const start = findPosition('S')
+  const end = findPosition('E')
+
+  const maze = new Map(entries.map(([key, value]) => [key, parseElement(value)]))
+
+  return {
+    start: start,
+    end: end,
+    maze: maze
+  }
 }
 
 function solve(input: PuzzleInput): Solution<bigint> {
