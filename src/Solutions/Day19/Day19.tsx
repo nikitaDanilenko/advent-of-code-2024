e8a9541b601021e1b090140d23df604f1a087b2f
Nikita Danilenko <nikita.danilenko@riskident.com>
2024-12-19 08:17:46 +0100

    Add parsing for day 19

--- src/Solutions/Day19/Day19.tsx
+++ src/Solutions/Day19/Day19.tsx
@@ -1,10 +1,23 @@
-import { Solution } from '../Utils/Types.ts'
+import {Solution} from '../Utils/Types.ts'
 import DayWith from '../Utils/DayUtil.tsx'
+import * as string_decoder from "node:string_decoder";
 
-type PuzzleInput = string
+type PuzzleInput = {
+  patterns: Pattern[]
+  towels: Towel[]
+}
+
+type Pattern = string
+type Towel = string
 
 function parse(input: string): PuzzleInput {
-  return input
+  const [patternString, towelString] = input.split('\n\n')
+  const patterns = patternString.split(', ')
+  const towels = towelString.split('\n')
+  return {
+    patterns: patterns,
+    towels: towels
+  }
 }
 
 function solve(input: PuzzleInput): Solution<bigint> {
