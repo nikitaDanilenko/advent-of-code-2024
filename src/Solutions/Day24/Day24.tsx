96c1b80522629ad714b0fbc725af1a8adf22a701
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-12-24 13:46:09 +0100

    Start work on today's task

--- src/Solutions/Day24/Day24.tsx
+++ src/Solutions/Day24/Day24.tsx
@@ -1,10 +1,66 @@
 import { Solution } from '../Utils/Types.ts'
 import DayWith from '../Utils/DayUtil.tsx'
 
-type PuzzleInput = string
+type PuzzleInput = {
+  initialValues: InitialValue[]
+  gates: Gate[]
+}
+
+type InitialValue = {
+  name: string
+  value: boolean
+}
+
+type Gate = {
+  input1: string
+  input2: string
+  output: string,
+  operation: Operation
+}
+
+enum Operation {
+  AND = 'AND',
+  OR = 'OR',
+  XOR = 'XOR'
+}
+
+function parseOperation(string: string): Operation {
+  switch (string) {
+    case Operation.AND:
+      return Operation.AND
+    case Operation.OR:
+      return Operation.OR
+    default:
+      return Operation.XOR
+  }
+}
 
 function parse(input: string): PuzzleInput {
-  return input
+  const [initialValuesBlock, gatesBlock] = input.split('\n\n')
+
+  const initialValues = initialValuesBlock.split('\n').map(line => {
+    const [name, value] = line.split(': ')
+    return {
+      name: name,
+      value: value === '1'
+    }
+  })
+
+  const gates = gatesBlock.split('\n').map(line => {
+    const [inputs, output] = line.split(' -> ')
+    const [input1, operation, input2] = inputs.split(' ')
+    return {
+      input1: input1,
+      input2: input2,
+      output: output,
+      operation: parseOperation(operation)
+    }
+  })
+
+  return {
+    initialValues: initialValues,
+    gates: gates
+  }
 }
 
 function solve(input: PuzzleInput): Solution<bigint> {
